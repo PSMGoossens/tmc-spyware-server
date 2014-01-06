e79eff7e5d1732520a8f010daaeaba78d3060ee3
Martin Pärtel <martin.partel@gmail.com>
2014-01-06 12:25:17 +0200

    Fix settings error messages.

--- settings.c
+++ settings.c
@@ -15,7 +15,7 @@ static long read_str(const char *envvar, const char **ptr)
 {
     const char *value = getenv(envvar);
     if (!value) {
-        fprintf(stderr, "Settings %s too long.\n", envvar);
+        fprintf(stderr, "Setting %s missing.\n", envvar);
         return -1;
     }
 
@@ -23,7 +23,7 @@ static long read_str(const char *envvar, const char **ptr)
     long remaining = SETTINGS_BUF_SIZE - (settings_buf_end - settings_buf);
 
     if (len > remaining) {
-        fprintf(stderr, "Settings %s too long.\n", envvar);
+        fprintf(stderr, "Settings too long.\n");
         return -2;
     }
 
@@ -37,12 +37,10 @@ static long read_str(const char *envvar, const char **ptr)
 int init_settings_from_env()
 {
     if (read_str("TMC_SPYWARE_DATA_DIR", &settings.data_dir) <= 0) {
-        fprintf(stderr, "Missing envvar TMC_SPYWARE_DATA_DIR\n");
         return 0;
     }
 
     if (read_str("TMC_SPYWARE_AUTH_URL", &settings.auth_url) <= 0) {
-        fprintf(stderr, "Missing envvar TMC_SPYWARE_AUTH_URL\n");
         return 0;
     }
 
